b2d063c67ae8394ae459c8d340809cd07ce80e85
Om Jadav <omjadav04@gmail.com>
2024-04-13 18:29:52 +0530

    url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chatapp-73xf.onrender.com", {
+      const socket = io("https://chatapp-ejqw.onrender.com", {
         // const socket = io("http://localhost:5000", {
         query: {
           userId: authUser._id,
